6d3a6e802d0a14d18ad70ab103546fe06524988f
rajatshetty10 <rajatshetty10@gmail.com>
2021-02-05 13:10:35 +0530

    data from local storage

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const fire = document.querySelector('.fire .fa-fire');
 const fireText = document.querySelector('#fire-text');
-var counter = 100;
+var counter = fireText.innerHTML;
+fireText.innerHTML = localStorage.getItem('clickcount');
 
+// Mouse Down
 fire.addEventListener('mousedown', () => {
-  fire.style.transform = 'scale(1.3)';
+  fire.style.transform = 'scale(1.1)';
   fire.style.transition = '0.5s ease';
-
-  counter++;
-  fireText.innerHTML = counter;
 });
 
+// Mouse Release
 fire.addEventListener('mouseup', () => {
   fire.style.transform = 'scale(1)';
   fire.style.transition = '0.5s ease';
 });
+
+// Store in local storage
+function clickCounter() {
+  if (typeof Storage !== 'undefined') {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    counter = localStorage.clickcount;
+    fireText.innerHTML = counter;
+  } else {
+    console.log('Browser does not support local storage');
+  }
+}
